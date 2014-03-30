4cea1fdb657cc6ca7a12463796122ddb40922646
satheesh kumar <pulsarcoder@gmail.com>
2014-03-31 00:01:45 +0530

    updated the database related files

--- app/Config/database.php
+++ app/Config/database.php
@@ -5,9 +5,9 @@ class DATABASE_CONFIG {
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'root',
-		'password' => '',
+		'host' => 'mydbinstance.czodqa8qz1pf.ap-southeast-2.rds.amazonaws.com',
+		'login' => 'admin',
+		'password' => 'development3mhz',
 		'database' => 'pixlposts',
 		'prefix' => ''
 	);
